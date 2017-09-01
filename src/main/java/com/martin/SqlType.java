71b604689cbe9a54ba55ad468eded8de412c0d56
chen.tengfei <chentf1836@163.com>
2017-09-01 10:03:22 +0800

    创建新工程

---
+++ src/main/java/com/martin/SqlType.java
@@ -0,0 +1,68 @@
+package com.martin;
+
+/**
+ * <Description> <br>
+ *
+ * @author chen.tengfei <br>
+ * @version 1.0<br>
+ * @CreateDate 2017/8/31 <br>
+ * @see com.martin <br>
+ * @since R9.0<br>
+ */
+public enum SqlType {
+
+    BIT("boolean"),
+
+    TINYINT("byte"),
+
+    SMALLINT("short"),
+
+    INT("int"),
+
+    BIGINT("Long"),
+
+    NUMBER("Long"),
+
+    FLOAT("float"),
+
+    DECIMAL("double"),
+
+    NUMERIC("double"),
+
+    REAL("double"),
+
+    MONEY("double"),
+
+    SMALLMONEY("double"),
+
+    VARCHAR2("String"),
+
+    VARCHAR("String"),
+
+    CHAR("String"),
+
+    NVARCHAR("String"),
+
+    NCHAR("String"),
+
+    TEXT("String"),
+
+    DATETIME("Date"),
+
+    DATE("Date"),
+
+    IMAGE("Blod");
+
+    /**
+     * 对应的Java类型
+     */
+    String type;
+
+    SqlType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
